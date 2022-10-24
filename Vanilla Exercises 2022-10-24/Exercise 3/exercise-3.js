9afce9d52705ef69ce29dbfc47df7a48dd62381c
Dennis Byberg <dennis.byberg@hotmail.com>
2022-10-24 23:19:45 +0200

    exercise 3 done.

---
+++ Vanilla Exercises 2022-10-24/Exercise 3/exercise-3.js
@@ -0,0 +1,5 @@
+/* let animals = ['cat', 'hamster', 'parrot', 'funky chihuahua']
+ console.log() how many objects the array above contains. */
+
+let animals = ["cat", "hamster", "parrot", "funky chihuahua"];
+console.log(animals.length); // psst...The correct answer is 4.
