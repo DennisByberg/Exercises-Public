cfe0fa39b2120b68999b581032ebd062a9dfde09
Dennis Byberg <dennis.byberg@hotmail.com>
2022-10-26 00:51:27 +0200

    -

--- Vanilla Exercises 2022-10-24/exercise 14/exercise-14.js
+++ Vanilla Exercises 2022-10-24/exercise 14/exercise-14.js
@@ -1,5 +1,5 @@
 let numArray = [23, 45, 5, 62, 1, 21, 3, 54];
-// In the array above, use a loop + if statement to filter out all numbers above 5 and insert them into a new array.
+// In the array above, use a loop + if statement to filter out all numbers above 5 and insert them into a new array...
 
 let newArray = [];
 
