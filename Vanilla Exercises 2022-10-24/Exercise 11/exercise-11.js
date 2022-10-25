2af70e9541ebdbd486f0f604655d4d6d4ad2cdce
Dennis Byberg <dennis.byberg@hotmail.com>
2022-10-25 18:04:31 +0200

    Klar med övning 11

--- Vanilla Exercises 2022-10-24/Exercise 11/exercise-11.js
+++ Vanilla Exercises 2022-10-24/Exercise 11/exercise-11.js
@@ -1,2 +1,11 @@
-let str = "Supenrcalifragilnisticexpialidocnious";
+let str = "Supercalifragilisticexpialidocious";
 // Check using a loop + if statement if the string above contains the letter n.
+
+let containsTheLetterN = false;
+for (let i = 0; i < str.length; i++) {
+  if (str[i] === "n" || str[i] === "N") {
+    containsTheLetterN = true;
+  }
+}
+
+console.log(containsTheLetterN);
