9f50cb82a09dd991704e4325d5c7f9a309697c3f
Dennis Byberg <dennis.byberg@hotmail.com>
2022-11-10 17:36:14 +0100

    @commit | Created working slider page

--- Solaris/scripts/script.js
+++ Solaris/scripts/script.js
@@ -1,11 +1,55 @@
+const BASE_URL = "https://fathomless-shelf-54969.herokuapp.com";
 const planets = document.querySelectorAll("main article");
-const hoverText = document.querySelector("#hover-text");
 
+getKey();
+getPlanets();
+
+/* HOVER PLANET */
 planets.forEach((planet) => {
   planet.addEventListener("mouseover", (event) => {
-    const planetName = event.target.id;
-    if (planetName != "sat-circle") {
-      hoverText.textContent = planetName;
-    }
+    ChangeTextOnHover(event);
   });
 });
+
+/* CLICK PLANET */
+planets.forEach((planet) => {
+  planet.addEventListener("click", (event) => {
+    toggleSlider();
+  });
+});
+
+/* FUNCTIONS */
+function ChangeTextOnHover(planets) {
+  const planetName = planets.target.id;
+  if (planetName != "sat-circle") {
+    document.querySelector("#hover-text").textContent = planetName;
+  }
+}
+
+// This function only toggle the slider to go back and fourth.
+function toggleSlider() {
+  slider.classList.toggle("show");
+}
+
+/* ASYNC FUNCTIONS */
+async function getKey() {
+  const response = await fetch(`${BASE_URL}/keys`, { method: "POST" });
+  // If everything goes as planed...
+  if (response.status === 200) {
+    const data = await response.json();
+    console.log(data);
+  } else {
+    // If not...?
+    console.error("ERROR");
+  }
+}
+
+async function getPlanets() {
+  const response = await fetch(`${BASE_URL}/bodies`, {
+    headers: {
+      "x-zocom": "solaris-vKkkQHqQboi7c6JF",
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+}
