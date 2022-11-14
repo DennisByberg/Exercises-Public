d45ecce132c1422b34391b1ccc1879b06253f242
Dennis Byberg <dennis.byberg@hotmail.com>
2022-11-14 15:14:13 +0100

    @commit | Ready to deploy version 1.0

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,17 +1,24 @@
-const myLeads = [
-  "www.awesomelead.com",
-  "www.epiclead.com",
-  "www.greatlead.com",
-];
+const myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
 
 document.querySelector("#input-btn").addEventListener("click", () => {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  inputEl.value = "";
+  renderLeads();
 });
 
-// Render the leads in the unordered list using ulEl.textContent
-for (let i = 0; i < myLeads.length; i++) {
-  ulEl.textContent += myLeads[i];
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    // Wrap the lead in an anchor tag (<a>) inside the <li>
+    // Can you make the link open in a new tab?
+    listItems += `
+      <li>
+        <a target="_blank" href="${myLeads[i]}">
+          ${myLeads[i]}
+        </a>
+      </li>`;
+  }
+  ulEl.innerHTML = listItems;
 }
