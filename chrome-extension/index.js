cce5ce63d8f977dee8c48f66d316f941509495d8
Dennis Byberg <dennis.byberg@hotmail.com>
2022-11-16 15:05:21 +0100

    @commit | #refactor.

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,34 +1,37 @@
 let myLeads = [];
+let oldLeads = [];
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage);
+const deleteBtn = document.querySelector("#delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
-document.querySelector("#input-btn").addEventListener("click", () => {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
-
-  // To verify that it works:
-  console.log(localStorage.getItem("myLeads"));
-});
-
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
       <li>
-        <a target="_blank" href="${myLeads[i]}">
-          ${myLeads[i]}
+        <a target="_blank" href="${leads[i]}">
+          ${leads[i]}
         </a>
       </li>`;
   }
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", () => {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+document.querySelector("#input-btn").addEventListener("click", () => {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
