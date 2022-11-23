c35b9065d54d378fa0f7823fee13a4db3515fdf9
Dennis Byberg <89915088+DennisByberg@users.noreply.github.com>
2022-11-23 13:12:30 +0100

    Delete Vanilla Exercises 2022-10-24 directory

--- Vanilla Exercises 2022-10-24/Exercise 09/exercise-09.js
+++
@@ -1,6 +0,0 @@
-let fruits = ["kiwi", "apple", "pear"];
-// In the array above, remove the last fruit in the array.
-
-fruits.pop();
-
-// console.log(fruits);
--- Vanilla Exercises 2022-10-24/exercise 16/exercise-16.js
+++
@@ -1,5 +0,0 @@
-// Create a loop that iterates 1000 turns. For each iteration you should console.log() which turn you are on.
-
-for (let i = 1; i <= 1000; i++) {
-  console.log(`You are on turn: ${i}`);
-}
