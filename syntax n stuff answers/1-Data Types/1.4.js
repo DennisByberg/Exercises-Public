5196a8f1af6e7e6eeb1ff2b123a91fbda26f38d5
Dennis Byberg <89915088+DennisByberg@users.noreply.github.com>
2022-11-23 13:12:44 +0100

    Delete syntax n stuff answers directory

--- syntax n stuff answers/1-Data Types/1.4.js
+++
@@ -1,6 +0,0 @@
-// How to find out the data type of a variable?
-
-const x = 10;
-console.log(typeof x);
-
-/* Answer ~> Using typeof. */
--- syntax n stuff answers/4-Arrays/4.1.js
+++
@@ -1,4 +0,0 @@
-// Make an array with 5 fruits in it.
-
-//Answer ~>
-const fruits = ["apple", "pear", "banana", "orange", "kiwi"];
