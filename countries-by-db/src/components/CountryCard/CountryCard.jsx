f72f689d9202ed6c5122d80155975dbe6a27a2dd
Dennis Byberg <dennis.byberg@hotmail.com>
2023-04-07 00:42:42 +0200

    implemented country card

--- countries-by-db/src/components/CountryCard/CountryCard.jsx
+++ countries-by-db/src/components/CountryCard/CountryCard.jsx
@@ -1,7 +1,8 @@
-const CountryCard = ({ name }) => {
+const CountryCard = ({ name, imageURL }) => {
   return (
-    <li>
-      <p>{name}</p>
+    <li className="country-card">
+      <p className="country-card__name">{name}</p>
+      <img className="country-card__flag" src={imageURL} />
     </li>
   );
 };
