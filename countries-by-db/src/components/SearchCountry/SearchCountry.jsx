1de138ef5b7cf03436a075af87c084ffac42600e
Dennis Byberg <dennis.byberg@hotmail.com>
2023-04-07 00:43:27 +0200

    implement functionality to the search function

--- countries-by-db/src/components/SearchCountry/SearchCountry.jsx
+++ countries-by-db/src/components/SearchCountry/SearchCountry.jsx
@@ -1,3 +1,5 @@
+// scss
+import "./SearchCountry.scss";
 //components
 import CountryCard from "../CountryCard/CountryCard";
 //other
@@ -5,25 +7,46 @@ import { useEffect, useState } from "react";
 
 const SearchCountry = () => {
   const [countries, setCountries] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [countryComponents, setCountryComponents] = useState([]);
 
   useEffect(() => {
-    async function getCountries() {
-      const allCountiesURL = "https://restcountries.com/v3.1/all";
-      const response = await fetch(allCountiesURL);
-      const data = await response.json();
-      setCountries(data);
+    if (searchInput) {
+      async function getCountries() {
+        const allCountiesURL = `https://restcountries.com/v3.1/name/${searchInput}`;
+        const response = await fetch(allCountiesURL);
+        const data = await response.json();
+        setCountries(data);
+      }
+      getCountries();
+    } else {
+      setCountryComponents([]);
     }
-    getCountries();
-  }, []);
+  }, [searchInput]);
 
-  const countryComponents = countries.map((country) => {
-    return <CountryCard key={country.name.common} name={country.name.common} />;
-  });
+  function handleSearchInput(event) {
+    setSearchInput(event.target.value);
+  }
+
+  useEffect(() => {
+    if (countries.length > 0) {
+      const searchedCountries = countries.map((country) => {
+        return (
+          <CountryCard
+            key={country.name.common}
+            name={country.name.common}
+            imageURL={country.flags.png}
+          />
+        );
+      });
+      setCountryComponents(searchedCountries);
+    }
+  }, [countries]);
 
   return (
-    <section>
-      <input type="text" />
-      <ul>{countryComponents}</ul>
+    <section className="search-country">
+      <input type="text" onChange={handleSearchInput} />
+      <ul className="search-country__ul">{countryComponents}</ul>
     </section>
   );
 };
